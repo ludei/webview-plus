8bc6abf92df971e99557f20a63ba3d04a6351c21
Carlos Hernández <carlos@ludei.com>
2014-06-25 12:26:02 +0200

    First public release.

--- README.md
+++ README.md
@@ -1,4 +1,89 @@
-Webview-
-========
+# Webview+ #
 
-Uniform webview on any Android 4.x device. 
+This project is a plugin for cordova apps, and provides a **uniform webview on any Android 4.x device.**.
+
+### Webview+ Features, Advantages and Benefits  ###
+
+* V8 Javascript VM
+* Works on Android 4.x
+* Latest HTML5 APIs
+* Compatible with Cordova Plugins
+* Better performance than system webview
+
+### How to install the Webview+ in your current project ###
+
+Even though it's a cordova-compatible plugin, some steps must be done for the installation. If you prefer, you can install it automatically using the [CocoonJS Command Line Interface](https://github.com/ludei/cocoonjs-cli). The cocoonjs-cli has the same usage and commands of [cordova-cli](https://github.com/apache/cordova-cli#project-commands).
+
+Once the cocoonjs-cli is installed in your system, just type:
+
+```
+$ cocoonjs plugin add com.ludei.webview.plus
+```
+
+### Manual installation  ###
+The CocoonJS-CLI automates the installation of the Webview+, but if for some reason you want to install the Webview+ without the CocoonJS-CLI you’ll have to do the following steps.
+
+These steps are specific for Cordova 3.4.0 and higher. If you want to install manually Webview+ in previous versions, please, refer to the hooks in the cocoonjs-cli
+
+**Step 1**
+
+Clone the Webview+ repository in your computer:
+
+```
+$ git checkout git@github.com:ludei/webview-plus.git
+```
+
+**Step 2**
+
+Edit the following file by using your favorite text editor
+
+```
+_PATH_TO_YOUR_CORDOVA_PROJECT_/platforms/android/CordovaLib/src/org/apache/cordova/CordovaWebview.java
+```
+
+**Step 3**
+
+Find the following string inside CordovaWebview.java:
+
+```
+public class CordovaWebView extends WebView
+```
+
+
+**Step 4**
+
+Replace the string you've found with these two strings:
+
+
+```
+import com.ludei.chromium.LudeiWebView;
+public class CordovaWebView extends LudeiWebView 
+```
+Save and close the file
+
+**Step 5**
+
+Edit the following file:
+
+```
+_PATH_TO_YOUR_CORDOVA_PROJECT_/platforms/android/CordovaLib/project.properties
+```
+
+**Step 6**
+
+Paste the following string and change the path after “android.library.reference.1=” to the relative path that points to your downloaded Webview+ (It must be a relative path and not an absolute path)
+
+```
+android.library.reference.1=../../../plugins/com.ludei.webview.plus/android
+```
+
+
+There you go. Now that you have installed the Webview+ in your project you can run the command “$ cordova build” and your app will benefit of the advantages of the Webview+.
+
+If you want to know how the CocoonJS-CLI does these steps programatically, check the sources at android/hooks folder (after_plugin_add / after_plugin_rm).
+
+### Known bugs ###
+
+* Launching the Webview+ inside android's emulator can lead to an application crash. Use a real device for testing.
+* Current version may not reproduce sound files. Will be fixed for next version
+* In some devices 3D context may not be created, this is due to a memory limitation of the device.
---
+++ android/hooks/after_plugin_rm/com.ludei.webview.plus.js
@@ -0,0 +1,141 @@
+#!/usr/bin/env node
+var task                    = require('child_process').exec,
+    fs                      = require('fs'),
+    pathLib                 = require('path'),
+    os                      = require('os'),
+    inWindows               = (os.platform() === 'win32');
+
+String.prototype.replaceAll = function(find, replace) {
+    var str = this;
+    return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
+};
+
+var App = function(plugin_name) {
+
+    if (!process.env.CORDOVA_PATH_BINARY || !process.env.CORDOVA_VERSION || !process.env.CORDOVA_PLUGIN_PATH) {
+        process.exit(0);
+    }
+
+    this.CORDOVA_PATH_BINARY = new Buffer(process.env.CORDOVA_PATH_BINARY, 'base64').toString();
+    this.CORDOVA_VERSION = new Buffer(process.env.CORDOVA_VERSION, 'base64').toString();
+    this.plugin_name = plugin_name;
+    this.plugin_path = this.getPath(new Buffer(process.env.CORDOVA_PLUGIN_PATH, 'base64').toString());
+    this.plugin_path_android = pathLib.join(this.plugin_path, "../", "android");
+    console.log("Path to Webview+ Project ", this.plugin_path_android, "Plugin path", this.plugin_path);
+};
+
+App.prototype = {
+
+    getPath: function(path) {
+        if (inWindows) {
+            return path.replaceAll('/', pathLib.sep);
+        } else {
+            return path;
+        }
+    },
+    getPlugins: function(callback) {
+        task(this.CORDOVA_PATH_BINARY + " plugins", function (error, stdout, stderr) {
+            try{
+                if(error) throw new Error(stderr);
+                if(this.ctx.CORDOVA_VERSION >= "3.5.0-0.1.0"){
+                    var tmp_list = [];
+                    stdout.split("\\n").forEach(function(plugin_info){
+                        tmp_list.push(plugin_info.split(" ")[0]);
+                    });
+                    stdout = JSON.stringify(tmp_list);
+                }
+                var plugins = JSON.parse(stdout.toString().replaceAll("'", '"'));
+                console.log("Plugin list: ", plugins);
+                if(Boolean(plugins) && Array.isArray(plugins))
+                {
+                    this.cllbck.call(this.ctx, plugins);
+                }else{
+                    this.cllbck.call(this.ctx, false);
+                }
+            }catch(e){
+                console.log(e);
+            }finally{
+                process.exit(1);
+            }
+        }.bind({ cllbck : callback, ctx : this }));
+    },
+
+    processPlugin: function(plugins) {
+        if( plugins.indexOf(this.plugin_name) !== -1 ){
+            this.removeChromium();
+        }
+    },
+    removeChromium: function() {
+
+        var CORDOVA_LIBRARY_PATH = this.getPath("./platforms/android/libs/cordova-3.2.0.jar");
+        var PROJECT_PROPERTIES_PATH = (this.CORDOVA_VERSION === "3.2.0-0.1.0") ? pathLib.join(process.cwd(), "platforms", "android", "project.properties") : pathLib.join(process.cwd(), "platforms" , "android", "CordovaLib" , "project.properties");
+        var CORDOVA_FRAMEWORK_WEBVIEW_PATH = this.getPath("./platforms/android/CordovaLib/src/org/apache/cordova/CordovaWebView.java");
+        var PROJECT_PROPERTIES_RELATIVE_PATH = (this.CORDOVA_VERSION === "3.2.0-0.1.0") ? process.cwd() + "/platforms/android/" : process.cwd() + "/platforms/android/CordovaLib/";
+        var RELATIVE_CHROMIUM_PATH = pathLib.relative(this.getPath(PROJECT_PROPERTIES_RELATIVE_PATH), this.getPath(this.plugin_path_android));
+        var project_properties_data = fs.readFileSync(PROJECT_PROPERTIES_PATH);
+
+        if ( project_properties_data.toString('utf-8').indexOf(this.plugin_name) !== -1 ) {
+
+            console.log("Removing Chromium" , PROJECT_PROPERTIES_PATH, "Cordova version", this.CORDOVA_VERSION);
+
+            if (this.CORDOVA_VERSION === "3.2.0-0.1.0") {
+                fs.renameSync(pathLib.join(this.plugin_path, "resources", "cordova-3.2.0.jar"), pathLib.join(CORDOVA_LIBRARY_PATH));
+                fs.renameSync(pathLib.join(process.cwd(), "platforms", "android", "libs", "cordova-ludei-framework.jar"), pathLib.join(this.plugin_path, "resources", "cordova-ludei-framework.jar"));
+                if (fs.existsSync(CORDOVA_LIBRARY_PATH)) {
+                    fs.unlinkSync(CORDOVA_LIBRARY_PATH);
+                }
+
+                var fileContents = fs.readFileSync(PROJECT_PROPERTIES_PATH).toString('utf-8');
+                var stringToDelete = "\nandroid.library.reference.1=" + RELATIVE_CHROMIUM_PATH.toString();
+
+                fs.writeFileSync(PROJECT_PROPERTIES_PATH, fileContents.replace(stringToDelete, ""));
+                console.log("Chromium deleted correctly.");
+            }
+
+            if(this.CORDOVA_VERSION >= "3.3.0-0.1.0"){
+                var cordova_webview_contents = fs.readFileSync(CORDOVA_FRAMEWORK_WEBVIEW_PATH).toString('utf-8');
+                var extend_original = "import com.ludei.chromium.LudeiWebView;\n public class CordovaWebView extends LudeiWebView";
+                var extend_end = "public class CordovaWebView extends WebView";
+
+                fs.unlinkSync(CORDOVA_FRAMEWORK_WEBVIEW_PATH);
+
+                fs.writeFileSync(CORDOVA_FRAMEWORK_WEBVIEW_PATH, cordova_webview_contents.replace(extend_original, extend_end));
+
+                var fileContents = fs.readFileSync(PROJECT_PROPERTIES_PATH).toString('utf-8');
+                var stringToDelete = "\nandroid.library.reference.1=" + RELATIVE_CHROMIUM_PATH.toString();
+
+                fs.writeFileSync(PROJECT_PROPERTIES_PATH, fileContents.replace(stringToDelete, ""));
+                console.log("Chromium deleted correctly.");
+            }
+
+            // Change the api level to the original one if needed
+            var android_manifest_path = pathLib.join( process.cwd() , "platforms", "android", "AndroidManifest.xml");
+            if(fs.existsSync(android_manifest_path)){
+                var manifest_data = fs.readFileSync(android_manifest_path).toString("utf8");
+                var manigest_reg_exp = /android:minSdkVersion="([^"]*)"/i;
+                var result = manifest_data.match(manigest_reg_exp);
+                if(result[1]){
+                    var android_api_level = parseInt(result[1]);
+                    if( android_api_level >= 14 ){
+                        manifest_data = manifest_data.replace(manigest_reg_exp, 'android:targetSdkVersion="10"');
+                        fs.writeFileSync(android_manifest_path, manifest_data, 'utf8');
+                    }
+                }
+            }else{
+                console.error("Cannot locate Android Manifest");
+            }
+
+        } else {
+            console.log("Webview+ isn't installed in the project. Stopping");
+        }
+
+        console.log("Hook execution finished.");
+        process.exit(0);
+    }
+}
+
+var cocoon_app = new App('com.ludei.webview.plus');
+
+cocoon_app.getPlugins(function(plugins) {
+    this.processPlugin(plugins);
+});
